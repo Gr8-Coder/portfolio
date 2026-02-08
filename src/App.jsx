e0d517bb1b402dabb5af76287800fce8daaa54cc
Sajal Tyagi <sajaltyagi@Sajals-MacBook-Air.local>
2026-02-08 09:58:59 +0530

    fix: Switch to HashRouter for GitHub Pages support

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
@@ -12,7 +12,7 @@ import { ThemeProvider } from './context/ThemeContext';
 function App() {
   return (
     <ThemeProvider>
-      <Router basename="/portfolio">
+      <Router>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
